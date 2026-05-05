ab603b3121f9b610de6be94ebdda467b5433ce16
David Giner <davidginer.alicante@gmail.com>
2026-05-05 17:15:39 +0400

    Ficheros completo

--- Practica2/src/main/java/Ficheros/Escritor.java
+++ Practica2/src/main/java/Ficheros/Escritor.java
@@ -1,6 +1,8 @@
 package Ficheros;
 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Escritor {
 
@@ -13,7 +15,7 @@ public class Escritor {
         //escritor.write("en un lugar de la Mancha");
 
         //escritor.close();
-        mayus();
+        combinar();
     }
 
     public static void mayus() throws IOException {
@@ -29,7 +31,7 @@ public class Escritor {
 
             for(String s : vector) {
 
-                escritor.write(s.substring(0,1).toUpperCase() + s.substring(1));
+                escritor.write(s.substring(0,1).toUpperCase() + s.substring(1) + " ");
             }
 
             escritor.newLine();
@@ -39,5 +41,60 @@ public class Escritor {
 
         lector.close();
         escritor.close();
+    }
+
+    public static void combinar() throws IOException{
+
+        BufferedReader archivo1 = new BufferedReader(new FileReader("src/main/java/Archivo1.txt"));
+        BufferedReader archivo2 = new BufferedReader(new FileReader("src/main/java/Archivo2.txt"));
+        BufferedWriter resultado = new BufferedWriter(new FileWriter("src/main/java/Resultado.txt"));
+
+        String linea;
+
+        Queue<String> cola1 = new LinkedList<>();
+        Queue<String> cola2 = new LinkedList<>();
+
+        while((linea = archivo1.readLine()) != null) {
+
+            String[] vector = linea.split(" ");
+
+            for(String s : vector) {
+
+                cola1.offer(s);
+            }
+        }
+
+        while((linea = archivo2.readLine()) != null) {
+
+            String[] vector = linea.split(" ");
+
+            for(String s : vector) {
+
+                cola2.offer(s);
+            }
+        }
+
+        int cola;
+
+        if(cola1.size() >= cola2.size()) {
+            cola = cola1.size();
+        }else{
+            cola = cola2.size();
+        }
+
+        for(int i = 0; i < cola; i++) {
+
+            if(!cola1.isEmpty()) {
+                resultado.write(cola1.poll() + " ");
+            }
+
+            if(!cola2.isEmpty()) {
+                resultado.write(cola2.poll() + " ");
+            }
+        }
+
+        archivo1.close();
+        archivo2.close();
+        resultado.close();
     }
 }
--- Practica2/src/main/java/Ficheros/FicherosPrueba.java
+++ Practica2/src/main/java/Ficheros/FicherosPrueba.java
@@ -1,8 +1,17 @@
 package Ficheros;
+import java.io.File;
 
 public class FicherosPrueba {
 
     public static void main(String[] args) {
+
+        File fichero = new File("src/main/java/ejemplo1.txt");
+
+        if(fichero.exists()) {
+            System.out.println("El fichero + " + fichero.getName() + " existe.");
+        }else{
+
+        }
 
     }
 }
